3e5d51ea724aac2037aa19eca13f7baf2b7cc3dc
Jose Luu <jose.luu@gmail.com>
2025-05-18 21:42:18 +0200

    document BLE library

--- mouse_automove.ino
+++ mouse_automove.ino
@@ -1,3 +1,19 @@
+// # this example is using the library https://github.com/T-vK/ESP32-BLE-Mouse
+// Go to https://github.com/T-vK/ESP32-BLE-Mouse.
+// Click the green Code button and select Download ZIP.
+// Save the ZIP file to your computer.
+// Install the Library in Arduino IDE:
+// Open the Arduino IDE.
+// Go to Sketch > Include Library > Add .ZIP Library.
+// Navigate to the downloaded ESP32-BLE-Mouse-master.zip file and select it.
+// The IDE will import the library.
+// Verify Installation:
+// Go to Sketch > Include Library > Contributed Libraries.
+// You should see ESP32-BLE-Mouse listed.
+// You can now include it in your sketch with #include <BleMouse.h>.
+
+// a good generic BLE tutorial is available here: https://randomnerdtutorials.com/esp32-bluetooth-low-energy-ble-arduino-ide/
+
 #include <BleMouse.h>
 
 BleMouse bleMouse("Mouse Jose");
